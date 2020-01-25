4d38a63bd54806db42fe043e5fd54e86f5e33053
Dima Suvorov <a9120294578@ya.ru>
2020-01-25 18:49:52 +0400

    work in intelliJ idea

---
+++ src/NanExample.java
@@ -0,0 +1,18 @@
+public class NanExample {
+
+    public static void main(String[] args) {
+
+        double d;
+
+        d = Math.sqrt(-100);
+        System.out.println(d);
+
+
+        int x = 10;
+        int y = ++x;
+        System.out.println(y);
+        System.out.println(x);
+
+        }
+
+}
