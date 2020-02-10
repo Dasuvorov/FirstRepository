b621a334570c9519b55bd10b3c1870f4c8a296f5
Dima Suvorov work <a9120294578@ya.ru>
2020-02-10 10:59:45 +0400

    add K1311 class

---
+++ K1311.java
@@ -0,0 +1,49 @@
+import java.util.Random;		
+		/**
+		Для проверки уровня загрязненности в реке было
+		выполнено 50 измерений, каждое из которых 
+		содержало информацию о том, в какой день
+		недели было произведено измерение, и каким был 
+		уровень загрязнения (по 10-балльной шкале).
+		Напишите класс, принимающий с клавиатуры данные 
+		50 измерений
+		(каждое измерение - два числа: первое - день недели,
+		 второе - уровень загрязнения) и выводящий на экран: 
+
+		• сообщение Опасно! - для дней, когда уровень загрязнения составлял 7 или более единиц;
+		• сообщение Спокойный вторник - для вторников, в которые
+		уровень загрязнения составлял менее 5 единиц.
+		*/
+
+public class K1311{
+	static Random random = new Random();
+	public static int measurements(int countMeasurements){
+			
+			
+			int i = 1 + random.nextInt(10);
+			
+			return i;
+	}
+	public static void pollution(int days, int poll){
+			
+		
+			if(poll >=7){
+				System.out.println("pollution " + poll + " swiming denger");
+			}
+			if(days == 2 & poll < 5){
+				System.out.println("days " + days + " calm  tuesday " + poll);
+			}
+	}
+	
+	public static void main(String[] args){
+	
+		for(int i = 1; i<=50; i++){
+			int measurements = measurements(i);
+			int days = 1 + random.nextInt(7);	
+			pollution( days, measurements);	
+		}
+	
+		
+		
+	}
+}
