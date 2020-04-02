c07d6f97aa0e9eb5a8bba357574a2e2f49d20dc8
Dima Suvorov <a9120294578@ya.ru>
2020-04-02 22:01:54 +0400

    modified K1527

--- src/K1527.java
+++ src/K1527.java
@@ -1,46 +1,48 @@
 import java.util.*;
 
 /**
-Напишите класс, который принимает с клавиатуры целое положительное
- число, а затем генерирует случайную цифру.
-Класс должен определить, встречается ли во введенном с клавиатуры
-числе сгенерированная цифра (сколько раз и на каких позициях), и
-вывести на экран соответствующую информацию. 
-*/
+ * Напишите класс, который принимает с клавиатуры целое положительное
+ * число, а затем генерирует случайную цифру.
+ * Класс должен определить, встречается ли во введенном с клавиатуры
+ * числе сгенерированная цифра (сколько раз и на каких позициях), и
+ * вывести на экран соответствующую информацию.
+ */
 
 public class K1527 {
-	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter number");
-		String number = scanner.nextLine();
-		Random random = new Random();
-		int r = random.nextInt(10);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number");
+        String number = scanner.nextLine();
+        Random random = new Random(85);
+        List<Integer> arrayList = new ArrayList<>();
+        int r = random.nextInt(10);
         System.out.println("enter r " + r);
-		char[] numberCh = number.toCharArray();
-		int[] numberInt = new int[numberCh.length];
-		int count = 0;
-		int i =0;
-		while(i<numberInt.length){
-			numberInt[i] = (int)numberCh[i] -48;
-			
-			i++;
-		}
-			
-			
-		for(i = 0; i < numberInt.length; i++){
+        char[] numberCh = number.toCharArray();
+        int[] numberInt = new int[numberCh.length];
+        int count = 0;
+        int i = 0;
+        while (i < numberInt.length) {
+            numberInt[i] = (int) numberCh[i] - 48;
 
-			if(numberInt[i] == r){
-				count++; 
-			System.out.print((i + 1) + " ");
-				
-			}
-			
-		}	
-		
-		System.out.println(" bingo " + count);
-		
-		
-	}
+            i++;
+        }
 
-    
+
+        for (i = 0; i < numberInt.length; i++) {
+
+            if (numberInt[i] == r) {
+                count++;
+
+                arrayList.add(i+1);
+            }
+
+        }
+        for (int x : arrayList) {
+            System.out.print(x + " ");
+        }
+        System.out.println("count " + count);
+
+    }
+
+
 }
